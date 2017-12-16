f8fb92c4a82ada5c5e35cab31669050d20923749
Aleksandr <hiphopkrv@gmail.com>
2017-12-16 12:51:51 +0200

    initial

---
+++ Notch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Notch.Startup))]
+namespace Notch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
