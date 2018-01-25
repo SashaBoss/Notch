26c764951f6779d4448ac9e9309d1cdf652b3cfa
Aleksandr Maha <maha.aleksandr@apriorit.com>
2018-01-25 18:47:56 +0200

    add orders table, shipped orders

--- Notch/Scripts/product.js
+++ Notch/Scripts/product.js
@@ -6,9 +6,9 @@
     self.Initialize = function (products) {
         var vmProducts = [];
         products.forEach(function(p) {
-            vmProducts.push(new Product(p.Id, p.Name, p.Price, p.Discount))
+            vmProducts.push(new Product(p.Id, p.Name, p.Price, p.Discount));
         });
-        ko.applyBindings(new ProductVM(products));
+        ko.applyBindings(new ProductVM(vmProducts));
     }
 
 }).apply(Product);
@@ -17,12 +17,15 @@ function ProductVM(products) {
     var self = this;
 
     self.AvailableProducts = products;
-
-    self.SetSelected = function() {
-        var p = this;
+    self.SelectedProduct = ko.observable({});
+    self.Order = ko.observable(new Order());
+    self.AddToOrder = function () {
+        self.Order().Products.push(self.SelectedProduct());
     }
 
-    self.SelectedProduct = ko.observable({});
+    self.RemoveProduct = function() {
+        self.Order().Products.remove(this);
+    }
 }
 
 function Product(id, name, price, discount) {
@@ -30,9 +33,27 @@ function Product(id, name, price, discount) {
     self.Id = id || 0;
     self.Name = name || "";
     self.Price = price || 0;
-    self.Discount = discount || 0;
+    self.Discount = ko.observable(discount);
 
-    self.TotalPrice = ko.computed(function () {
-        return self.Price * self.Discount;
+    self.DiscountPrice = ko.computed(function () {
+        return self.Price - (self.Price * self.Discount() /100);
+    });
+}
+
+function Order() {
+    var self = this;
+    self.Products = ko.observableArray();
+    self.OrderTotal = ko.computed(function() {
+        var total = 0;
+
+        self.Products().forEach(function(product) {
+            total += product.DiscountPrice();
+        });
+
+        return total;
+    });
+
+    self.ProductsCount = ko.computed(function () {
+        return self.Products().length;
     });
 }
